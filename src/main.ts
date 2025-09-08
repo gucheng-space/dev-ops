f2e07c6efeae6837b0995747c6e4b4467a6a746d
gucheng-space <gucheng25218@outlook.com>
2025-09-08 22:28:26 +0800

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const post = process.env.POST || 3000;
+  app.setGlobalPrefix('api');
+  // 校验
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('DevOps API')
+    .setDescription('DevOps 接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(post);
+  console.log(`Swagger is running on http://localhost:${post}/api/docs`);
 }
 bootstrap();
